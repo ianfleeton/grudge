6cf760d5f904ab4c76bf1d977a46b3e40d3f9291
Ian Fleeton <ianfleeton@gmail.com>
2010-07-24 13:07:21 +0100

    Parse UML element declarations with layout

--- scripts/layout-render/SpecificationParser.groovy
+++ scripts/layout-render/SpecificationParser.groovy
@@ -21,8 +21,10 @@ class SpecificationParser {
     def parse(textSpec) {
         diagramSpecification = new DiagramSpecification()
         textSpec.eachLine {
-            if(it.contains('-')) {
+            if(it.contains(']-[')) {
                 parseAssociation(it);
+            } else if(it.contains('{')) {
+                parseDeclarationWithLayout(it);
             } else {
                 parseDeclaration(it);
             }
@@ -40,8 +42,19 @@ class SpecificationParser {
     def parseDeclaration(line) {
         def matcher = line =~ /\w+/
         def element = new UMLClassElement()
-        println matcher[0]
         element.name = matcher[0]
         elementsMap[matcher[0]] = element
+    }
+
+    def parseDeclarationWithLayout(line) {
+        // Floating point regex from http://www.regular-expressions.info/floatingpoint.html
+        def matcher = line =~ /\[(\w+)\{rect:([-+]?[0-9]*\.?[0-9]+),([-+]?[0-9]*\.?[0-9]+),([-+]?[0-9]*\.?[0-9]+),([-+]?[0-9]*\.?[0-9]+)\}\]/
+        def element = new UMLClassElement()
+        element.name = matcher[0][1]
+        elementsMap[matcher[0][1]] = element
+        element.topLeft.x = matcher[0][2].toFloat()
+        element.topLeft.y = matcher[0][3].toFloat()
+        element.bottomRight.x = matcher[0][4].toFloat()
+        element.bottomRight.y = matcher[0][5].toFloat()
     }
 }
